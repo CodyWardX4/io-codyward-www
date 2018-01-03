a45bc342a3be0dce1f53ecc0653db6550e97703b
Ward <WardC11@michigan.gov>
2018-01-03 15:14:35 -0500

    started basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('myAwesomeAnimation', [
+      
+      state('small', style({
+        transform: 'scale(1)',
+      })),
+
+      state('large', style({
+        transform: 'scale(1.2)',
+      })),
+
+      transition('small <=> large', animate('300ms ease-in')), 
+    ]),
+    trigger('menuSmoothSlide', [
+      state('collapsed', style({
+        color: 'red'
+      })),
+      state('expanded', style({
+        color: 'green',
+      })),
+
+      transition('collapsed <=> expanded', animate('300ms ease-in')),
+    ])
+  ]
 })
 export class AppComponent {
+  
+  state: string = 'small'
+
+  animateMe() {
+    this.state = (this.state === 'small' ? 'large' : 'small');
+  }
+
+  animateMenu() {
+    this.state = (this.state === 'collapsed' ? 'expanded' : 'collapsed');
+  }
+
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
+import { ConnectComponent } from './connect/connect.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    ContentComponent,
+    ConnectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
